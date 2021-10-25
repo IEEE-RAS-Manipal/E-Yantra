61cfbe0e043cf117b27041e71ea3512c0d2ca98e
CH13F-1419 <neehalsharrma1419@outlook.com>
2021-10-25 18:29:12 +0530

    Cleaned up with PyLint

--- Workspace_Template/catkin_ws/src/strawberry_stacker/task_1/scripts/ArUco_library.py
+++ Workspace_Template/catkin_ws/src/strawberry_stacker/task_1/scripts/ArUco_library.py
@@ -1,99 +1,84 @@
 #!/usr/bin/env python3
-############## Task1.1 - ArUco Detection ##############
+'''
+This module is a library for ArUco detection in OpenCV.
+'''
 
-import numpy as np
-import cv2
-import cv2.aruco as aruco
-import sys
 import math
-import time
+import cv2
+from cv2 import aruco
 
 def detect_ArUco(img):
-	## function to detect ArUco markers in the image using ArUco library
-	## argument: img is the test image
-	## return: dictionary named Detected_ArUco_markers of the format {ArUco_id_no : corners}, where ArUco_id_no indicates ArUco id and corners indicates the four corner position of the aruco(numpy array)
-	## 		   for instance, if there is an ArUco(0) in some orientation then, ArUco_list can be like
-	## 				{0: array([[315, 163],
-	#							[319, 263],
-	#							[219, 267],
-	#							[215,167]], dtype=float32)}
-
+    '''
+    Detecting the Arucos in the image and extracting ID and coordinate values.
+    '''
     Detected_ArUco_markers = {}
-    ## enter your code here ##
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_250)
     parameters = aruco.DetectorParameters_create()
     corners, ids, _ = aruco.detectMarkers(gray, aruco_dict, parameters = parameters)
-    
+
     # verify *at least* one ArUco marker was detected
     if len(corners)>0 :
-			# flatten the ArUco IDs list
-      ids = ids.flatten()
-			# loop over the detected ArUCo corners
-    for (markerCorner, markerID) in zip(corners, ids):
-      Detected_ArUco_markers[markerID] = markerCorner
-   
+            # flatten the ArUco IDs list
+        ids = ids.flatten()
+            # loop over the detected ArUCo corners
+    for (marker_corner, marker_id) in zip(corners, ids):
+        Detected_ArUco_markers[marker_id] = marker_corner
+
     return Detected_ArUco_markers
 
 
 def Calculate_orientation_in_degree(Detected_ArUco_markers):
-	## function to calculate orientation of ArUco with respective to the scale mentioned in problem statement
-	## argument: Detected_ArUco_markers  is the dictionary returned by the function detect_ArUco(img)
-	## return : Dictionary named ArUco_marker_angles in which keys are ArUco ids and the values are angles (angles have to be calculated as mentioned in the problem statement)
-	##			for instance, if there are two ArUco markers with id 1 and 2 with angles 120 and 164 respectively, the 
-	##			function should return: {1: 120 , 2: 164}
-
-	ArUco_marker_angles = {}
-	## enter your code here ##
-	c = 0
-	for key,coordinates in Detected_ArUco_markers.items():
-		global x0,x1,x2,x3,y0,y1,y2,y3,mpx,mpy,cx,cy
-		print(Detected_ArUco_markers[key][0][0])
-		x0, y0 = map(int, Detected_ArUco_markers[key][0][0])
-		x1, y1 = map(int, Detected_ArUco_markers[key][0][1])
-		x2, y2 = map(int, Detected_ArUco_markers[key][0][2])
-		x3, y3 = map(int, Detected_ArUco_markers[key][0][3])
-		mpx, mpy = map(int,(((x0+x1)/2) , ((y0+y1)/2 )) )    #calculating midpoint between topleft and topright
-		cx = int((x0+x2)/2)
-		cy = int((y0+y2)/2 )
-		angle = int(math.degrees(math.atan2(mpy-cy,mpx-cx)))
+    '''
+    Calculates the orientation of each detected ArUco.
+    '''
+    ArUco_marker_angles = {}
+    for key in Detected_ArUco_markers.items():
+        print(Detected_ArUco_markers[key][0][0])
+        x_0, y_0 = map(int, Detected_ArUco_markers[key][0][0])
+        x_1, y_1 = map(int, Detected_ArUco_markers[key][0][1])
+        x_2, y_2 = map(int, Detected_ArUco_markers[key][0][2])
+        #calculating midpoint between topleft and topright
+        mpx, mpy = map(int, (((x_0+x_1)/2), ((y_0+y_1)/2)))
+        c_x = int((x_0+x_2)/2)
+        c_y = int((y_0+y_2)/2 )
+        angle = int(math.degrees(math.atan2(mpy-c_y,mpx-c_x)))
 
 
-		if angle>0:
-			angle = 360-angle
-		else:
-			angle = abs(angle)
-		ArUco_marker_angles[key] = angle
-
-	return ArUco_marker_angles	## returning the angles of the ArUco markers in degrees as a dictionary
+        if angle>0:
+            angle = 360-angle
+        else:
+            angle = abs(angle)
+        ArUco_marker_angles[key] = angle
+    #returning the angles of the ArUco markers in degrees as a dictionary
+    return ArUco_marker_angles
 
 
 def mark_ArUco(img,Detected_ArUco_markers,ArUco_marker_angles):
-	## function to mark ArUco in the test image as per the instructions given in problem statement
-	## arguments: img is the test image 
-	##			  Detected_ArUco_markers is the dictionary returned by function detect_ArUco(img)
-	##			  ArUco_marker_angles is the return value of Calculate_orientation_in_degree(Detected_ArUco_markers)
-	## return: image namely img after marking the aruco as per the instruction given in problem statement
-
+    '''
+    Creates masks over the ArUcos with angle, ID and other data.
+    '''
     ## enter your code here ##
-    for arucoID,angle in ArUco_marker_angles.items():
-      x0, y0 = map(int, Detected_ArUco_markers[arucoID][0][0])
-      x1, y1 = map(int, Detected_ArUco_markers[arucoID][0][1])
-      x2, y2 = map(int, Detected_ArUco_markers[arucoID][0][2])
-      x3, y3 = map(int, Detected_ArUco_markers[arucoID][0][3])
-      mpx, mpy = map(int,(((x0+x1)/2) , ((y0+y1)/2 )) )    #calculating midpoint between topleft and topright
-      cx = int((x0+x2)/2)
-      cy = int((y0+y2)/2 )
-      cv2.circle(img, (x0,y0) , 5, (125,125,125), -1)              #gray dot
-      cv2.circle(img, (x1,y1) , 5, (0,255,0), -1)                  #green dot
-      cv2.circle(img, (x2,y2) , 5, (180,105,255), -1)              #pink dot
-      cv2.circle(img, (x3,y3) , 5, (255,255,255), -1)              #white dot
-      cv2.circle(img, (cx,cy) , 5, (0,0,255), -1)                  #red dot at the centre
-      cv2.line(img, (cx,cy), (mpx,mpy), (255,0,0), 5)              #blue line connecting the centre and the midpoint
-		
-      cv2.putText(img, str(arucoID), (cx+50,cy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)          #printing ID on the right of centre
-      cv2.putText(img, str(angle), (cx-100,cy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)            #printing orientation on left of centre
-      cv2.putText(img, "SS_1302", (40,40), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,0,255), 2)               #printing the team ID
+    for aruco_id,angle in ArUco_marker_angles.items():
+        x_0, y_0 = map(int, Detected_ArUco_markers[aruco_id][0][0])
+        x_1, y_1 = map(int, Detected_ArUco_markers[aruco_id][0][1])
+        x_2, y_2 = map(int, Detected_ArUco_markers[aruco_id][0][2])
+        x_3, y_3 = map(int, Detected_ArUco_markers[aruco_id][0][3])
+        #calculating midpoint between topleft and topright
+        mpx, mpy = map(int,(((x_0+x_1)/2), ((y_0+y_1)/2)))
+        c_x = int((x_0+x_2)/2)
+        c_y = int((y_0+y_2)/2 )
+        cv2.circle(img, (x_0,y_0) , 5, (125,125,125), -1)   #gray dot
+        cv2.circle(img, (x_1,y_1) , 5, (0,255,0), -1)       #green dot
+        cv2.circle(img, (x_2,y_2) , 5, (180,105,255), -1)   #pink dot
+        cv2.circle(img, (x_3,y_3) , 5, (255,255,255), -1)   #white dot
+        cv2.circle(img, (c_x,c_y) , 5, (0,0,255), -1)       #red dot at the centre
+        cv2.line(img, (c_x,c_y), (mpx,mpy), (255,0,0), 5)   #blue line connecting centre and midpt
+
+        #printing ID on the right of centre
+        cv2.putText(img, str(aruco_id), (c_x+50,c_y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+        #printing orientation on left of centre
+        cv2.putText(img, str(angle), (c_x-100,c_y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+        #printing the team ID
+        cv2.putText(img, "SS_1302", (40,40), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,0,255), 2)
     return img
-
-
