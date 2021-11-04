5c2b3bbd07287dff8a87ed3b2a05b1f2e528343e
CH13F-1419 <neehalsharrma1419@outlook.com>
2021-11-04 15:22:28 +0530

    Code optimisation
    Cleaned up the dict access pipeline and the angle calculation pipeline.

--- SS_1302_WS/src/SS_1302_PKGS/ss_1302_task_1/scripts/ss_1302_marker_detection.py
+++ SS_1302_WS/src/SS_1302_PKGS/ss_1302_task_1/scripts/ss_1302_marker_detection.py
@@ -23,7 +23,7 @@ class ImageProc():
         # This will contain your image frame from camera
         self.img = np.empty([])
         self.bridge = CvBridge()
-        self.detected_aruco_markers = []
+        self.detected_aruco_markers = {}
 
         # Initialise rosnode
         rospy.init_node('marker_detection', anonymous=True)
@@ -38,11 +38,10 @@ class ImageProc():
         self.image_sub = rospy.Subscriber(
             "/camera/camera/image_raw", Image, self.image_callback)
 
-    def detect_aruco(img):
+    def detect_aruco(self, img):
         '''
         Detecting the Arucos in the image and extracting ID and coordinate values.
         '''
-        detected_aruco_markers = {}
         corners = []
         ids = []
         aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_250)
@@ -56,9 +55,7 @@ class ImageProc():
             ids = ids.flatten()
             # loop over the detected ArUCo corners
             for (marker_corner, marker_id) in zip(corners, ids):
-                detected_aruco_markers[marker_id] = marker_corner
-
-        return detected_aruco_markers
+                self.detected_aruco_markers[marker_id] = marker_corner
 
     def image_callback(self, data):
         '''
@@ -81,11 +78,11 @@ class ImageProc():
         '''
         Calculates the position and orientation of the marker (centre position).
         '''
-        for key, _ in self.detected_aruco_markers.items():
-            # Calculating relevante point coordinates
-            x_0, y_0 = map(int, self.detected_aruco_markers[key][0][0])
-            x_1, y_1 = map(int, self.detected_aruco_markers[key][0][1])
-            x_2, y_2 = map(int, self.detected_aruco_markers[key][0][2])
+        for key, coods in self.detected_aruco_markers.items():
+            # Calculating relevant point coordinates
+            x_0, y_0 = map(int, coods[0][0])
+            x_1, y_1 = map(int, coods[0][1])
+            x_2, y_2 = map(int, coods[0][2])
 
             # Calculating centre position
             c_x = int((x_0+x_2)/2)
@@ -93,11 +90,9 @@ class ImageProc():
 
             # Calculating orientation angle
             mpx, mpy = map(int, (((x_0+x_1)/2), ((y_0+y_1)/2)))
-            angle = int(math.degrees(math.atan2(mpy-c_y, mpx-c_x)))
-            if angle > 0:
-                angle = 360-angle
-            else:
-                angle = abs(angle)
+            angle = int(math.degrees(math.atan2(c_y-mpy, c_x-mpx)))
+            if angle < 0:
+                angle = 360+angle
 
             # Filling in message data
             self.marker_msg.x = c_x
@@ -108,17 +103,13 @@ class ImageProc():
 
 
 if __name__ == '__main__':
-    '''
-    Controls the overall execution.
-    '''
-    image_proc_obj = ImageProc() # Creating object
-    image_proc_obj.rate.sleep() # Delay for syncing
+    image_proc_obj = ImageProc()  # Creating object
+    image_proc_obj.rate.sleep()  # Delay for syncing
     try:
-        while not rospy.is_shutdown(): # Will run till node active
-            image_proc_obj.rate.sleep() # Delay
+        while not rospy.is_shutdown():  # Will run till node active
+            image_proc_obj.rate.sleep()  # Delay
             # Detecting ArUco marker
-            image_proc_obj.detected_aruco_markers = ImageProc.detect_aruco(
-                image_proc_obj.img)
+            image_proc_obj.detect_aruco(image_proc_obj.img)
             # Calculating and publishing position/orientation data
             image_proc_obj.marker_pos()
     except ROSInterruptException:
